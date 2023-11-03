ddcf23bf49d0b16b0f2db61aedba842f8644fa83
Gusto <bacvinka@gmail.com>
2023-11-03 18:34:56 +0200

    Move generated libvinilla.h to target dir

--- vinilla-lib/build.rs
+++ vinilla-lib/build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let root_dir = PathBuf::from("../".to_string());
+    let root_dir = PathBuf::from("../target/".to_string());
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
